28ff95fb132b9be5383a5c02101a0bcbd48832e3
Holy-Morphism <ranaadeeltahir@gmail.com>
2023-09-07 01:03:59 +0500

    Define entity classes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
