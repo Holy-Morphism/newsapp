d6258df53b4f405ab9aadaa401b68fc8d7c219d5
Holy-Morphism <ranaadeeltahir@gmail.com>
2023-09-09 13:33:56 +0500

    updated all dependencies

---
+++ lib/features/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:newsapp/core/constants/contstants.dart';
+import 'package:newsapp/features/data/models/article.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'news_api_service.g.dart';
+
+@RestApi(baseUrl: newsAPIBaseURL)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
+
+  @GET('/top-headlines')
+  Future<HttpResponse<List<ArticleModel>>> getNewsArticle({
+    @Query("apiKey") String? apiKey,
+    @Query("country") String? country,
+    @Query("category") String? category,
+  });
+}
